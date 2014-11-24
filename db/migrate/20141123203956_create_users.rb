338a2601f75cf3c318276e7978effb77813ed907
RafehSaeed <rafeh_saeed@hotmail.ca>
2014-11-23 21:24:58 -0500

    added stuff

---
+++ db/migrate/20141123203956_create_users.rb
@@ -0,0 +1,11 @@
+gclass CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :type
+
+      t.timestamps null: false
+    end
+  end
+end
